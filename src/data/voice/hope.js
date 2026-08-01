effbaa1d99c53489d67c3df8b1d5b9b8870c713f
mjeykey <marinaswelt31@gmail.com>
2026-08-01 22:13:29 +0100

    Add hope voice exercises

---
+++ src/data/voice/hope.js
@@ -0,0 +1,13 @@
+export const hopeVoice = {
+  id: 'hope',
+  title: 'Hope',
+  emoji: '🌅',
+  exercises: [
+    { sentence: 'Amanhã pode trazer algo bom.', english: 'Tomorrow may bring something good.', alternative: 'Amanhã pode ser melhor.', alternativeEnglish: 'Tomorrow can be better.' },
+    { sentence: 'Ainda há caminhos que eu não conheço.', english: 'There are still paths I do not know.', alternative: 'Ainda há novos caminhos.', alternativeEnglish: 'There are still new paths.' },
+    { sentence: 'Um dia difícil não dura para sempre.', english: 'A difficult day does not last forever.', alternative: 'Este dia vai passar.', alternativeEnglish: 'This day will pass.' },
+    { sentence: 'Posso voltar a acreditar em coisas boas.', english: 'I can believe in good things again.', alternative: 'Posso voltar a acreditar.', alternativeEnglish: 'I can believe again.' },
+    { sentence: 'Há mais possibilidades do que consigo ver.', english: 'There are more possibilities than I can see.', alternative: 'Ainda há possibilidades.', alternativeEnglish: 'There are still possibilities.' },
+    { sentence: 'Continuar também é uma forma de esperança.', english: 'Continuing is also a form of hope.', alternative: 'Eu continuo com esperança.', alternativeEnglish: 'I continue with hope.' }
+  ]
+};
