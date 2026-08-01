e147900ac54b6491cd730956b2b7eeee5ea5e707
mjeykey <marinaswelt31@gmail.com>
2026-08-01 16:16:35 +0100

    Refresh Story module for all-page language data

--- src/app/main.js
+++ src/app/main.js
@@ -1,10 +1,10 @@
-import { createStore } from './state.js?v=6';
-import { renderMenu } from '../modules/menu.js?v=6';
-import { renderWords } from '../modules/words.js?v=6';
-import { renderMemory } from '../modules/memory.js?v=6';
-import { renderFillGap } from '../modules/fill-gap.js?v=6';
-import { renderStory } from '../modules/story.js?v=6';
-import { stopSpeech } from '../audio/speech.js?v=6';
+import { createStore } from './state.js?v=7';
+import { renderMenu } from '../modules/menu.js?v=7';
+import { renderWords } from '../modules/words.js?v=7';
+import { renderMemory } from '../modules/memory.js?v=7';
+import { renderFillGap } from '../modules/fill-gap.js?v=7';
+import { renderStory } from '../modules/story.js?v=7';
+import { stopSpeech } from '../audio/speech.js?v=7';
 
 const root = document.getElementById('app');
 const store = createStore();
