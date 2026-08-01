c39070d1553107d8b03e0779105c8280588b8227
mjeykey <marinaswelt31@gmail.com>
2026-08-01 13:07:38 +0100

    Add central transform module registry

---
+++ src/transforms/index.js
@@ -0,0 +1,21 @@
+(() => {
+  'use strict';
+
+  window.SpeakUPTransforms = window.SpeakUPTransforms || {};
+
+  window.SpeakUPTransforms.applyAll = function applyAll(source) {
+    let html = String(source || '');
+
+    const orderedTransforms = [
+      window.SpeakUPTransforms.improveWordsMode
+    ];
+
+    for (const transform of orderedTransforms) {
+      if (typeof transform === 'function') {
+        html = transform(html);
+      }
+    }
+
+    return html;
+  };
+})();
