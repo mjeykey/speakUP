12fc84122ef8bf2acfe6465e981513ef6a96485c
mjeykey <marinaswelt31@gmail.com>
2026-08-02 13:56:12 +0100

    Load French language registry in menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,5 +1,5 @@
 import { STORIES } from '../data/content.js?v=4';
-import { LANGUAGE_OPTIONS } from '../data/language-content-extended.js?v=1';
+import { LANGUAGE_OPTIONS } from '../data/language-content-extended.js?v=2';
 
 const MODES = [
   ['fill-gap', 'Sentences', 'Complete sentences in your selected learning language.'],
