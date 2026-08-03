0e26472cfafe5df45598ffa30b188d4d67f99287
mjeykey <marinaswelt31@gmail.com>
2026-08-03 17:57:06 +0100

    Load centralized progress modules

--- src/app/main.js
+++ src/app/main.js
@@ -1,11 +1,11 @@
-import { createStore } from './state.js?v=54';
+import { createStore } from './state.js?v=55';
 import { renderWelcome } from '../modules/welcome.js?v=52';
 import { renderMenu } from '../modules/menu.js?v=63';
-import { renderWords } from '../modules/words.js?v=52';
-import { renderMemory } from '../modules/memory.js?v=59';
+import { renderWords } from '../modules/words.js?v=53';
+import { renderMemory } from '../modules/memory.js?v=60';
 import { renderFillGap } from '../modules/fill-gap.js?v=53';
 import { renderSpeakPractice } from '../modules/speak-practice.js?v=54';
-import { renderStory } from '../modules/story-loader.js?v=61';
+import { renderStory } from '../modules/story-loader.js?v=62';
 import { renderEffectsSettings } from '../modules/effects-settings.js?v=52';
 import { renderEmotions } from '../modules/emotions.js?v=53';
 import { renderFuture } from '../modules/future.js?v=1';
