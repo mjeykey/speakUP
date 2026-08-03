6d07612211518fa8858acc574a1a17627550d90b
mjeykey <marinaswelt31@gmail.com>
2026-08-03 17:26:27 +0100

    Load saved story and multilingual emotions

--- src/app/main.js
+++ src/app/main.js
@@ -1,13 +1,13 @@
-import { createStore } from './state.js?v=53';
+import { createStore } from './state.js?v=54';
 import { renderWelcome } from '../modules/welcome.js?v=52';
 import { renderMenu } from '../modules/menu.js?v=63';
 import { renderWords } from '../modules/words.js?v=52';
 import { renderMemory } from '../modules/memory.js?v=59';
 import { renderFillGap } from '../modules/fill-gap.js?v=53';
 import { renderSpeakPractice } from '../modules/speak-practice.js?v=54';
-import { renderStory } from '../modules/story-loader.js?v=60';
+import { renderStory } from '../modules/story-loader.js?v=61';
 import { renderEffectsSettings } from '../modules/effects-settings.js?v=52';
-import { renderEmotions } from '../modules/emotions.js?v=52';
+import { renderEmotions } from '../modules/emotions.js?v=53';
 import { renderFuture } from '../modules/future.js?v=1';
 import { stopSpeech } from '../audio/speech.js?v=58';
 
