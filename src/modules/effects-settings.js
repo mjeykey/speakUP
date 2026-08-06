ab902ab652a898a45e334420557dee61f5e06c45
mjeykey <marinaswelt31@gmail.com>
2026-08-06 16:15:35 +0100

    Load particel effect

--- src/modules/effects-settings.js
+++ src/modules/effects-settings.js
@@ -4,7 +4,7 @@ import {
   explodeText,
   getModeTextEffect,
   setModeTextEffect
-} from '../effects/text-effects.js?v=3';
+} from '../effects/text-effects.js?v=4';
 
 export function renderEffectsSettings(root, store) {
   root.innerHTML = `<section class="screen effects-settings-screen">
