5d071f223314992e3181e3893e6b8c2d7e5836d6
mjeykey <marinaswelt31@gmail.com>
2026-08-06 16:35:22 +0100

    Refresh fractured particel effect

--- src/modules/effects-settings.js
+++ src/modules/effects-settings.js
@@ -4,7 +4,7 @@ import {
   explodeText,
   getModeTextEffect,
   setModeTextEffect
-} from '../effects/text-effects.js?v=5';
+} from '../effects/text-effects.js?v=6';
 
 export function renderEffectsSettings(root, store) {
   root.innerHTML = `<section class="screen effects-settings-screen">
