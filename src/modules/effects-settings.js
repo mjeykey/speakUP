e5119752168cb5821e0849b6dbf30ea459fa67ac
mjeykey <marinaswelt31@gmail.com>
2026-08-07 12:47:39 +0100

    Bump particel effect cache version

--- src/modules/effects-settings.js
+++ src/modules/effects-settings.js
@@ -4,7 +4,7 @@ import {
   explodeText,
   getModeTextEffect,
   setModeTextEffect
-} from '../effects/text-effects.js?v=7';
+} from '../effects/text-effects.js?v=8';
 
 export function renderEffectsSettings(root, store) {
   root.innerHTML = `<section class="screen effects-settings-screen">
