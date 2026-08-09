a5706026d5a0d755b14b64c9f77fbb368120bedb
mjeykey <marinaswelt31@gmail.com>
2026-08-09 14:58:08 +0100

    Expose crack effect in effect settings

--- src/modules/effects-settings.js
+++ src/modules/effects-settings.js
@@ -4,7 +4,7 @@ import {
   explodeText,
   getModeTextEffect,
   setModeTextEffect
-} from '../effects/distinct-text-effects.js?v=4';
+} from '../effects/distinct-text-effects.js?v=5';
 
 export function renderEffectsSettings(root, store) {
   root.innerHTML = `<section class="screen effects-settings-screen">
