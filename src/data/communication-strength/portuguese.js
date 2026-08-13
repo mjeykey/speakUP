8483f7109c0c27d20d531f9684cb36a806215070
mjeykey <marinaswelt31@gmail.com>
2026-08-13 21:53:32 +0100

    Add Portuguese Speak Strong pack

---
+++ src/data/communication-strength/portuguese.js
@@ -0,0 +1,14 @@
+export const PORTUGUESE_STRONG = [
+  ['Desculpa incomodar, mas será que posso fazer uma pergunta?','Tenho uma pergunta.'],
+  ['Não sei se isto faz sentido, mas…','É assim que eu vejo a situação.'],
+  ['Desculpa, mas não posso fazer isso.','Não posso fazer isso.'],
+  ['Talvez pudéssemos tentar outra opção?','Vamos tentar outra opção.'],
+  ['Está tudo bem. Não te preocupes.','Na verdade, isso não foi confortável para mim.'],
+  ['Não quero parecer indelicada, mas discordo.','Eu vejo isso de outra forma.'],
+  ['Se não der muito trabalho, podes enviar hoje?','Por favor, envia hoje.'],
+  ['Posso estar errada, mas acho que precisamos de mais tempo.','Acho que precisamos de mais tempo.'],
+  ['Desculpa, talvez não tenha explicado bem.','Deixa-me esclarecer o que quero dizer.'],
+  ['Espero que não leves a mal, mas preciso de algum espaço.','Preciso de algum espaço agora.'],
+  ['Posso tentar arranjar maneira de encaixar isso.','Hoje não tenho capacidade para isso.'],
+  ['Desculpa, provavelmente estou completamente errada.','Este é o meu entendimento; corrige-me se for necessário.']
+];
