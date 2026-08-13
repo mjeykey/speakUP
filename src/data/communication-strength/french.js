ca067552224515dd3aa0d2436c870f95a0ec0f43
mjeykey <marinaswelt31@gmail.com>
2026-08-13 21:54:24 +0100

    Add French Speak Strong pack

---
+++ src/data/communication-strength/french.js
@@ -0,0 +1,14 @@
+export const FRENCH_STRONG = [
+  ['Désolé de te déranger, mais est-ce que je peux te poser une question ?','J’ai une question.'],
+  ['Je ne sais pas si ça a du sens, mais…','Voilà comment je vois les choses.'],
+  ['Je suis désolé, mais je ne peux pas faire ça.','Je ne peux pas faire ça.'],
+  ['On pourrait peut-être essayer une autre option ?','Essayons une autre option.'],
+  ['Ce n’est rien. Ne t’inquiète pas.','En fait, ça ne m’a pas convenu.'],
+  ['Je ne veux pas paraître impoli, mais je ne suis pas d’accord.','Je vois les choses autrement.'],
+  ['Si ça ne te dérange pas trop, tu pourrais l’envoyer aujourd’hui ?','Merci de l’envoyer aujourd’hui.'],
+  ['Je me trompe peut-être, mais je pense qu’il nous faut plus de temps.','Je pense qu’il nous faut plus de temps.'],
+  ['Désolé, je me suis peut-être mal expliqué.','Je vais préciser ce que je veux dire.'],
+  ['J’espère que tu ne le prendras pas mal, mais j’ai besoin d’un peu d’espace.','J’ai besoin d’un peu d’espace maintenant.'],
+  ['Je peux essayer de trouver un moyen de le caser.','Je n’ai pas la capacité de m’en charger aujourd’hui.'],
+  ['Je me trompe peut-être sur ce point.','Voilà ce que j’ai compris ; corrige-moi si nécessaire.']
+];
