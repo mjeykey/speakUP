416031b9fca0db246efe5ce2a82bc6fe35f24d03
mjeykey <marinaswelt31@gmail.com>
2026-08-13 21:56:24 +0100

    Add English Speak Strong pack

---
+++ src/data/communication-strength/english.js
@@ -0,0 +1,10 @@
+export const ENGLISH_STRONG = [
+  ['Sorry to bother you, but could I maybe ask something?','I have a question.'],
+  ["I don't know if this makes sense, but...","Here's how I see it."],
+  ["I'm sorry, but I can't do that.","I can't do that."],
+  ['Maybe we could possibly try another option?',"Let's try another option."],
+  ["It's fine. Don't worry about it.","Actually, that didn't feel okay to me."],
+  ["I don't want to sound rude, but I disagree.",'I see it differently.'],
+  ['If it is not too much trouble, could you send it today?','Please send it today.'],
+  ['I might be wrong, but I think we need more time.','I think we need more time.']
+];
