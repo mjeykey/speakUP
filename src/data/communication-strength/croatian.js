ebe3005ab1d3b488002c1280158640fe1ef2e0fa
mjeykey <marinaswelt31@gmail.com>
2026-08-13 21:55:25 +0100

    Add Croatian Speak Strong pack

---
+++ src/data/communication-strength/croatian.js
@@ -0,0 +1,10 @@
+export const CROATIAN_STRONG = [
+  ['Oprosti što smetam, mogu li nešto pitati?','Imam pitanje.'],
+  ['Ne znam ima li ovo smisla, ali…','Ovako ja to vidim.'],
+  ['Oprosti, ali ne mogu to napraviti.','Ne mogu to napraviti.'],
+  ['Možda bismo mogli probati drugu opciju?','Probajmo drugu opciju.'],
+  ['U redu je, ne brini.','Zapravo, meni to nije bilo u redu.'],
+  ['Ne želim zvučati nepristojno, ali ne slažem se.','Ja to vidim drugačije.'],
+  ['Ako nije problem, možeš li to poslati danas?','Molim te, pošalji to danas.'],
+  ['Možda griješim, ali mislim da nam treba više vremena.','Mislim da nam treba više vremena.']
+];
