79d56c657cb302c6161abc7a07bd0dea10e54f87
mjeykey <marinaswelt31@gmail.com>
2026-08-13 21:54:14 +0100

    Add Spanish Speak Strong pack

---
+++ src/data/communication-strength/spanish.js
@@ -0,0 +1,14 @@
+export const SPANISH_STRONG = [
+  ['Perdona que moleste, pero ¿te puedo hacer una pregunta?','Tengo una pregunta.'],
+  ['No sé si esto tiene sentido, pero…','Así lo veo yo.'],
+  ['Lo siento, pero no puedo hacer eso.','No puedo hacer eso.'],
+  ['Quizá podríamos intentar otra opción.','Probemos otra opción.'],
+  ['No pasa nada. No te preocupes.','En realidad, eso no me pareció bien.'],
+  ['No quiero sonar descortés, pero no estoy de acuerdo.','Yo lo veo de otra manera.'],
+  ['Si no es mucha molestia, ¿podrías enviarlo hoy?','Por favor, envíalo hoy.'],
+  ['Puede que me equivoque, pero creo que necesitamos más tiempo.','Creo que necesitamos más tiempo.'],
+  ['Perdona, quizá no lo he explicado bien.','Déjame aclarar lo que quiero decir.'],
+  ['Espero que no te lo tomes a mal, pero necesito un poco de espacio.','Ahora necesito un poco de espacio.'],
+  ['Puedo intentar encajarlo de alguna manera.','Hoy no tengo disponibilidad para eso.'],
+  ['Puede que lo haya entendido mal.','Esto es lo que entiendo; corrígeme si hace falta.']
+];
