95df4e3a405a04dad02fa76f26bc92379e237672
mjeykey <marinaswelt31@gmail.com>
2026-08-16 19:28:28 +0100

    Load smoother speech engine in Story Mode

--- src/modules/story.js
+++ src/modules/story.js
@@ -1,7 +1,7 @@
 import { getMultilingualStory } from '../data/stories/multilingual-stories.js?v=1';
 import { fantasyStory } from '../data/stories/fantasy.js?v=3';
 import { getFantasyTranslation } from '../data/stories/fantasy-translations.js?v=1';
-import { speak, stopSpeech } from '../audio/speech.js?v=62';
+import { speak, stopSpeech } from '../audio/speech.js?v=63';
 import { isStorySfxPlaying, preloadStorySfx, playStorySfx, stopStorySfx } from '../audio/story-sfx-simple.js?v=10';
 import { getSpeechLanguage, languageName } from '../data/language-content-matrix.js?v=1';
 
