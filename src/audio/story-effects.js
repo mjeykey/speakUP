7251c17108e0a352a78c9be05ae63d00fe8f6ffe
mjeykey <marinaswelt31@gmail.com>
2026-08-23 20:11:07 +0100

    Use amplified door engine

--- src/audio/story-effects.js
+++ src/audio/story-effects.js
@@ -1,4 +1,4 @@
-import { isStorySfxPlaying, preloadStorySfx, playStorySfx, setStorySfxVolume, stopStorySfx } from './story-sfx.js?v=198';
+import { isStorySfxPlaying, preloadStorySfx, playStorySfx, setStorySfxVolume, stopStorySfx } from './story-sfx.js?v=200';
 import { isDoorCreakPlaying, playDoorCreak, stopDoorCreak, unlockDoorCreak } from './story-door.js?v=2';
 
 const BELL_NORMAL_VOLUME=.90;
