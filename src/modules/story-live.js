3a2756b0cbfbd9acffca9aaacc010661dfde0545
mjeykey <marinaswelt31@gmail.com>
2026-08-23 21:09:35 +0100

    Refresh glass effect build

--- src/modules/story-live.js
+++ src/modules/story-live.js
@@ -10,7 +10,7 @@ const CHURCH_BELL_PAGES=new Set([1]);
 const DOOR_CREAK_PAGES=new Set([2]);
 const VERIFIED_DOOR_URL=new URL('../../assets/audio/freesound_community-heavy-metal-door-74594.mp3?v=205',import.meta.url).href;
 let verifiedDoorAudio=null;
-const DEBUG_BUILD='B205';
+const DEBUG_BUILD='B206';
 const escapeHtml=value=>String(value??'').replaceAll('&','&amp;').replaceAll('<','&lt;').replaceAll('>','&gt;').replaceAll('"','&quot;').replaceAll("'",'&#039;');
 const shuffle=items=>[...items].sort(()=>Math.random()-.5);
 
