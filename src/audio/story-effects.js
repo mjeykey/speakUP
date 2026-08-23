6ed4406e0208f27c85ef9de59145a6a760655892
mjeykey <marinaswelt31@gmail.com>
2026-08-23 20:53:17 +0100

    Refresh verified door build

--- src/audio/story-effects.js
+++ src/audio/story-effects.js
@@ -1,4 +1,4 @@
-import { isStorySfxPlaying, preloadStorySfx, playStorySfx, setStorySfxVolume, stopStorySfx } from './story-sfx.js?v=203';
+import { isStorySfxPlaying, preloadStorySfx, playStorySfx, setStorySfxVolume, stopStorySfx } from './story-sfx.js?v=204';
 import { isDoorCreakPlaying, playDoorCreak, stopDoorCreak, unlockDoorCreak } from './story-door.js?v=2';
 
 const BELL_NORMAL_VOLUME=.90;
