b0f3a12f4d13a347a0a611869f30b008b50c5ad7
mjeykey <marinaswelt31@gmail.com>
2026-08-24 22:17:24 +0100

    Keep rain playing with falling rocks on pages 77-80

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -1,9 +1,11 @@
 import { renderStory as renderBaseStory } from './story-live.js?v=232';
 import { installScene7376 } from '../audio/story-scene-73-76.js?v=242';
 import { installScene7780 } from '../audio/story-scene-77-80.js?v=246';
+import { installRain7780 } from '../audio/story-rain-77-80.js?v=247';
 
 export function renderStory(root,store){
   installScene7376(root,store);
   installScene7780(root,store);
+  installRain7780(root,store);
   return renderBaseStory(root,store);
 }
