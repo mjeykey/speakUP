c1ad60553e2622ea6b775d69530cc7fca0955b3e
mjeykey <marinaswelt31@gmail.com>
2026-08-24 19:57:42 +0100

    Load persistent wagon audio build

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -1,5 +1,5 @@
 import { renderStory as renderBaseStory } from './story-live.js?v=232';
-import { installScene7376 } from '../audio/story-scene-73-76.js?v=235';
+import { installScene7376 } from '../audio/story-scene-73-76.js?v=236';
 
 export function renderStory(root,store){
   installScene7376(root,store);
