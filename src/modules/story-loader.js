269c7201d571a6556c05e79ff7ec64850cacb84f
mjeykey <marinaswelt31@gmail.com>
2026-08-24 22:20:25 +0100

    Refresh quieter rain modules

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -1,7 +1,7 @@
-import { renderStory as renderBaseStory } from './story-live.js?v=232';
+import { renderStory as renderBaseStory } from './story-live.js?v=248';
 import { installScene7376 } from '../audio/story-scene-73-76.js?v=242';
 import { installScene7780 } from '../audio/story-scene-77-80.js?v=246';
-import { installRain7780 } from '../audio/story-rain-77-80.js?v=247';
+import { installRain7780 } from '../audio/story-rain-77-80.js?v=248';
 
 export function renderStory(root,store){
   installScene7376(root,store);
