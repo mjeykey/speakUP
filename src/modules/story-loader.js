01ccb64e2ac1d170e54ed2366c479beef4b96d3b
mjeykey <marinaswelt31@gmail.com>
2026-08-24 19:47:42 +0100

    Install dedicated pages 73-76 audio scene

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -1,1 +1,7 @@
-export { renderStory } from './story-live.js?v=231';
+import { renderStory as renderBaseStory } from './story-live.js?v=232';
+import { installScene7376 } from '../audio/story-scene-73-76.js?v=232';
+
+export function renderStory(root,store){
+  installScene7376(root,store);
+  return renderBaseStory(root,store);
+}
