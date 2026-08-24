6a85188194371a03000e02e6c80a6dea12a77ad4
mjeykey <marinaswelt31@gmail.com>
2026-08-24 23:24:37 +0100

    Remove unused crowd cleanup wrapper

--- src/data/stories/fantasy-clean.js
+++
@@ -1,6 +0,0 @@
-import { fantasyStory as sourceFantasyStory } from './fantasy.js?v=3';
-
-export const fantasyStory={
-  ...sourceFantasyStory,
-  pages:sourceFantasyStory.pages.map(page=>page.sound==='crowd'?{...page,sound:'none'}:page)
-};
