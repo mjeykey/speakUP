798f2dfe36368675dbe8828f78faed992a45d400
mjeykey <marinaswelt31@gmail.com>
2026-08-24 19:51:45 +0100

    Load simplified wagon-only scene audio

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -1,5 +1,5 @@
 import { renderStory as renderBaseStory } from './story-live.js?v=232';
-import { installScene7376 } from '../audio/story-scene-73-76.js?v=232';
+import { installScene7376 } from '../audio/story-scene-73-76.js?v=234';
 
 export function renderStory(root,store){
   installScene7376(root,store);
