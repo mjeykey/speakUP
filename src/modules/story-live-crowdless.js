13a04481727576e7f268032450ba4e458a4a1e0b
mjeykey <marinaswelt31@gmail.com>
2026-08-24 23:58:04 +0100

    Load dedicated rain story build 268

--- src/modules/story-live-crowdless.js
+++ src/modules/story-live-crowdless.js
@@ -1,5 +1,5 @@
 import { fantasyStory } from '../data/stories/fantasy.js?v=3';
-import { renderStory as renderBaseStory } from './story-live.js?v=267';
+import { renderStory as renderBaseStory } from './story-live.js?v=268';
 
 function applyStorySoundOverrides(){
   fantasyStory.pages.forEach(page=>{
