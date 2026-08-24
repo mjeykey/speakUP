f565a2822b7549e97c43a7e57148b9ea24caa8f7
mjeykey <marinaswelt31@gmail.com>
2026-08-24 22:48:20 +0100

    Refresh stone crack scene module

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -2,7 +2,7 @@ import { renderStory as renderBaseStory } from './story-live.js?v=250';
 import { installScene7376 } from '../audio/story-scene-73-76.js?v=242';
 import { installScene7780 } from '../audio/story-scene-77-80.js?v=246';
 import { installRain7780 } from '../audio/story-rain-77-80.js?v=250';
-import { installScene8992 } from '../audio/story-scene-89-92.js?v=251';
+import { installScene8992 } from '../audio/story-scene-89-92.js?v=252';
 
 export function renderStory(root,store){
   installScene7376(root,store);
