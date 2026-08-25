9c47a4f408f414974036725b6b2656a15ec14425
mjeykey <marinaswelt31@gmail.com>
2026-08-25 19:57:14 +0100

    Bump wagon storm scene to build 279

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -3,7 +3,7 @@ import { installScene7376 } from '../audio/story-scene-73-76.js?v=275';
 import { installScene7780 } from '../audio/story-scene-77-80.js?v=246';
 import { installScene8184 } from '../audio/story-scene-81-84.js?v=276';
 import { installScene8992Direct } from '../audio/story-scene-89-92-direct.js?v=254';
-import { installScene153156 } from '../audio/story-scene-153-156.js?v=278';
+import { installScene153156 } from '../audio/story-scene-153-156.js?v=279';
 
 export function renderStory(root,store){
   installScene7376(root,store);
