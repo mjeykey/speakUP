e502e974c8985c3c9aff80aae0cfe5138fce037d
mjeykey <marinaswelt31@gmail.com>
2026-08-25 20:55:21 +0100

    Disable music and ambience on pages 157-158

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -4,6 +4,7 @@ import { installScene7780 } from '../audio/story-scene-77-80.js?v=246';
 import { installScene8184 } from '../audio/story-scene-81-84.js?v=276';
 import { installScene8992Direct } from '../audio/story-scene-89-92-direct.js?v=254';
 import { installScene153156 } from '../audio/story-scene-153-156.js?v=281';
+import { installSilence157158 } from '../audio/story-silence-157-158.js?v=282';
 
 export function renderStory(root,store){
   installScene7376(root,store);
@@ -11,5 +12,6 @@ export function renderStory(root,store){
   installScene8184(root,store);
   installScene8992Direct(root,store);
   installScene153156(root,store);
+  installSilence157158(root);
   return renderBaseStory(root,store);
 }
