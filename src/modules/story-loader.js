e631783da197ad2ed0ef913dcb0029d3107615d2
mjeykey <marinaswelt31@gmail.com>
2026-08-25 18:51:55 +0100

    Install wagon storm scene on pages 153-156

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -1,13 +1,15 @@
-import { renderStory as renderBaseStory } from './story-live-crowdless.js?v=276';
+import { renderStory as renderBaseStory } from './story-live-crowdless.js?v=277';
 import { installScene7376 } from '../audio/story-scene-73-76.js?v=275';
 import { installScene7780 } from '../audio/story-scene-77-80.js?v=246';
 import { installScene8184 } from '../audio/story-scene-81-84.js?v=276';
 import { installScene8992Direct } from '../audio/story-scene-89-92-direct.js?v=254';
+import { installScene153156 } from '../audio/story-scene-153-156.js?v=277';
 
 export function renderStory(root,store){
   installScene7376(root,store);
   installScene7780(root,store);
   installScene8184(root,store);
   installScene8992Direct(root,store);
+  installScene153156(root,store);
   return renderBaseStory(root,store);
 }
