195c9e5a8daf2fe37939a851b57a63eda98a95cd
mjeykey <marinaswelt31@gmail.com>
2026-08-31 10:51:53 +0100

    Load fight grunts scene on pages 189-192

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -7,6 +7,7 @@ import { installScene153156 } from '../audio/story-scene-153-156.js?v=283';
 import { installScene157160 } from '../audio/story-scene-157-160.js?v=285';
 import { installScene169172 } from '../audio/story-scene-169-172.js?v=285';
 import { installScene173176 } from '../audio/story-scene-173-176.js?v=285';
+import { installScene189192FightGrunts } from '../audio/story-scene-189-192-fight-grunts.js?v=295';
 
 export function renderStory(root,store){
   installScene7376(root,store);
@@ -17,5 +18,6 @@ export function renderStory(root,store){
   installScene157160(root,store);
   installScene169172(root,store);
   installScene173176(root,store);
+  installScene189192FightGrunts(root,store);
   return renderBaseStory(root,store);
 }
