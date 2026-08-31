cf38640bc26993f82be202cf87e52be7239f05e8
mjeykey <marinaswelt31@gmail.com>
2026-08-31 18:05:59 +0100

    Wire page 213 claw impact scene

--- src/modules/story-loader.js
+++ src/modules/story-loader.js
@@ -8,6 +8,7 @@ import { installScene157160 } from '../audio/story-scene-157-160.js?v=285';
 import { installScene169172 } from '../audio/story-scene-169-172.js?v=285';
 import { installScene173176 } from '../audio/story-scene-173-176.js?v=285';
 import { installScene189192FightGrunts } from '../audio/story-scene-189-192-fight-grunts.js?v=298';
+import { installScene213ClawImpact } from '../audio/story-scene-213-claw-impact.js?v=299';
 
 export function renderStory(root,store){
   installScene7376(root,store);
@@ -19,5 +20,6 @@ export function renderStory(root,store){
   installScene169172(root,store);
   installScene173176(root,store);
   installScene189192FightGrunts(root,store);
+  installScene213ClawImpact(root,store);
   return renderBaseStory(root,store);
 }
